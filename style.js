55a17d5e3c2a3c62a0c4aed52249a49e9478d3a3
Hossam Gamal Elhelw <91392770+HossamGamalElhelw@users.noreply.github.com>
2023-02-19 00:17:17 +0200

    Update style.js

--- style.js
+++ style.js
@@ -1,1 +1,73 @@
-const btnConfirm = document.querySelector(".btn-confirm");
+const btnConfirm = document.getElementById("btn_confirm"),
+  inputs = document.querySelectorAll("input[type='number']"),
+  cardNumber = document.querySelector("#card_number"),
+  cardFrontNum = document.querySelector(".card-front-num"),
+  cardOwner = document.querySelector(".card-owner"),
+  cardName = document.querySelector("#card_name"),
+  cardMoth = document.querySelector("#card_month"),
+  cardYear = document.querySelector("#card_year"),
+  cardDate = document.querySelector(".card-data"),
+  cardCvc = document.querySelector("#card_cvc"),
+  cardPassword = document.querySelector(".card-password"),
+  errorMsg = document.querySelectorAll(".error-msg"),
+  correctMsg = document.querySelector(".thank"),
+  rightSide = document.querySelector(".right_section");
+
+cardName.addEventListener("input", (event) => {
+  const inputText = event.target.value;
+  cardOwner.textContent = inputText;
+});
+cardNumber.addEventListener("input", (event) => {
+  const inputText = event.target.value;
+  cardFrontNum.textContent = inputText.replace(/(\d{4})/g, "$1 ");
+});
+cardMoth.addEventListener("input", (event) => {
+  const inputText = event.target.value;
+  cardDate.textContent = inputText;
+});
+cardYear.addEventListener("input", (event) => {
+  const inputText = event.target.value;
+  cardDate.textContent = cardMoth.value + "/" + inputText;
+});
+
+cardCvc.addEventListener("input", (event) => {
+  const inputText = event.target.value;
+  cardPassword.textContent = inputText;
+});
+
+function checkInputs() {
+  let isEmpty = false;
+  if (cardName.value === "") {
+    isEmpty = true;
+    errorMsg[0].style.display = "block";
+  }
+  if (cardNumber.value === "" || cardNumber.value.length < 14) {
+    isEmpty = true;
+    errorMsg[1].style.display = "block";
+  }
+  if (cardYear.value === "") {
+    isEmpty = true;
+    errorMsg[2].style.display = "block";
+  }
+  if (cardDate.value === "") {
+    isEmpty = true;
+    errorMsg[2].style.display = "block";
+  }
+  if (cardCvc.value === "") {
+    isEmpty = true;
+    errorMsg[2].style.display = "block";
+  }
+  return isEmpty;
+}
+
+btnConfirm.addEventListener("click", () => {
+  const isEmpty = checkInputs();
+  if (!isEmpty) {
+    rightSide.style.display = "none";
+    correctMsg.style.display = "block";
+  }
+  setInterval(() => {
+    rightSide.style.display = "none";
+    correctMsg.style.display = "block";
+  }, 5000);
+});
